5fd6e4bec3ee9045f90debd0df9b2c29e8d648a3
Nura Alam Rifat <nuraalamrifat@gmail.com>
2024-06-05 17:45:29 +0600

    editing: transaction list

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,11 +1,30 @@
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
-        <li class="minus">
-          Cash <span>-$400</span><button class="delete-btn">x</button>
-        </li>
-        <li class="plus">
-          Paycheck <span>$400</span><button class="delete-btn">x</button>
+        <li
+            :class="transaction.amount < 0 ? 'minus' : 'plus'"
+            v-for="transaction in transactions"
+            :key="transaction.id"
+        >
+            {{ transaction.text }} <span>${{ transaction.amount }}</span
+            ><button class="delete-btn">x</button>
         </li>
     </ul>
 </template>
+
+<script>
+export default {
+    setup() {
+        const transactions = [
+            { id: 1, text: "Flower", amount: -19.99 },
+            { id: 2, text: "Salary", amount: 500 },
+            { id: 3, text: "Book", amount: -10.99 },
+            { id: 4, text: "Camera", amount: 150 },
+        ];
+
+        return {
+            transactions,
+        };
+    },
+};
+</script>
